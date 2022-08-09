9c1eb6393c56668af727157f3733edac700287a7
karthigasun <prkarthik3@gmail.com>
2022-08-09 16:55:33 +0530

    js-practice

---
+++ program-for-largenumber/script.js
@@ -0,0 +1,18 @@
+let a,b,c;
+
+a=parseInt(prompt("Given a value"));
+b=parseInt(prompt("Given b value"));
+c=parseInt(prompt("Given c value"))
+
+if(a>b){
+
+    if(a>c){
+        console.log("First value is greater");
+    }
+}
+else 
+    {if(b>c)
+    console.log("Second value is greater");
+
+    else{ console.log("Third value is greater");}
+}
