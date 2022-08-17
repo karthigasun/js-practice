b647b92e2347deb2658c9df24773143741620c90
karthigasun <prkarthik3@gmail.com>
2022-08-17 16:55:24 +0530

    js-practice

--- print-prime-numbers/script.js
+++ print-prime-numbers/script.js
@@ -4,21 +4,23 @@ let n,i,a,b;
 n= parseInt(prompt("Give the Value"));
 
 console.log(n);
+a=0;
 
 
-for (i=0; i <=n; i++) {
-    a=0;
+for (i=2;n>i;i++) {
+    if (n%i==0) {
+         a=1;
+        break;
+}
+}  
+if(a==1){
 
-    // looping through 2 to user input number
-    for (b=2;b<i;b++) {
-        if (i%b==0) {
-            a=1;
-            break;
-        }
-    }  
+    console.log("Not prime number");
+}
+else{
 
-    // if number greater than 1 and not divisible by other numbers
-    if (i>1 &&a==0) {
-        console.log(i);
-    }
-}    
+    console.log("Prime Number");
+}
+   
+
+
